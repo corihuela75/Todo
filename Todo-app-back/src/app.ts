29cdec149609adb8a65e947905665161335972a0
Micaela Orellano <miqa.orellano@gmail.com>
2025-11-13 12:05:22 -0300

    Cambios en back por Mongo Atlas

--- Todo-app-back/src/app.ts
+++ Todo-app-back/src/app.ts
@@ -1,17 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 import todoRoutes from './routes/todo.routes';
+
+
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // Conexión a MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/todoapp')
-  .then(() => console.log('✅ MongoDB conectado'))
+
+const mongoURI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/todoapp";
+mongoose.connect(mongoURI)
+  .then(() => console.log('MongoDB conectado'))
   .catch(err => console.log(err));
 
 app.use('/api/todos', todoRoutes);
 
-app.listen(3000, () => console.log('🚀 Server running at http://localhost:3000'));
+app.listen(PORT, () => console.log('Server running at http://localhost:{PORT}'));
