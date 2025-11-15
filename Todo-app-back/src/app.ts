acd155e3a37bdd094aa5c4e98c7ff6cd1c49f6a8
Sofi <sm.pereyra95@gmail.com>
2025-11-15 18:01:21 -0300

    [backend] separo listen y connect del flujo de inicializacion de servidor para evitar conflictos con testing

--- Todo-app-back/src/app.ts
+++ Todo-app-back/src/app.ts
@@ -1,17 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import todoRoutes from './routes/todo.routes';
+import express from "express";
+import cors from "cors";
+import todoRoutes from "./routes/todo.routes";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use("/api/todos", todoRoutes);
 
-// Conexión a MongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/todoapp')
-  .then(() => console.log('✅ MongoDB conectado'))
-  .catch(err => console.log(err));
-
-app.use('/api/todos', todoRoutes);
-
-app.listen(3000, () => console.log('🚀 Server running at http://localhost:3000'));
+export default app;
